1b41a1f1617199ca3b083e88002188eea4fa8cf6
LEESANGNAM <fgh0116@naver.com>
2023-07-27 00:29:15 +0900

    풀다운버튼&초기화

--- Emotion/ResultViewController.swift
+++ Emotion/ResultViewController.swift
@@ -8,11 +8,12 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    
     
     @IBOutlet var resultLabels: [UILabel]!
     let emotionEnum = Emotion.allCases
     
+    @IBOutlet weak var pullDownButton: UIButton!
     
     @IBOutlet var labelBackgroundViews: [UIView]!
     
@@ -20,6 +21,7 @@ class ResultViewController: UIViewController {
         super.viewDidLoad()
         setUpViews()
         setUpLabel()
+        setUpPulldownButton()
         // Do any additional setup after loading the view.
     }
     // 탭바 넘어갈때 willappear 호출
@@ -63,7 +65,52 @@ class ResultViewController: UIViewController {
         }
     }
     
-    @IBAction func resetButtonTapped(_ sender: UIButton) {
+    func setUpPulldownButton(){
+        let firstButton = UIAction(title: "지수 모두 초기화") { _ in
+            self.emotionResetAll()
+        }
+        let secondButton = UIAction(title: "완전행복지수 초기화") { _ in
+            self.emotionReset(emotion: .veryHappy)
+        }
+        let thirdButton = UIAction(title: "적당미소지수 초기화") { _ in
+            self.emotionReset(emotion: .happy)
+        }
+        let fourthButton = UIAction(title: "그냥그냥지수 초기화") { _ in
+            self.emotionReset(emotion: .soso)
+        }
+        let fifthButton = UIAction(title: "속상한지수 초기화") { _ in
+            self.emotionReset(emotion: .bad)
+        }
+        let sixthButton = UIAction(title: "많이슬픈지수 초기화") { _ in
+            self.emotionReset(emotion: .sad)
+        }
+        let buttonMenu = UIMenu(children:
+        [
+            firstButton,secondButton,thirdButton,
+            fourthButton,fifthButton,sixthButton
+        ])
+        pullDownButton.menu = buttonMenu
+    }
+
+    func emotionReset(emotion: Emotion){
+        UserDefaults.standard.set(0, forKey: Emotion.getString(emotion)())
+        guard let index = emotionEnum.firstIndex(of: emotion) else { print("오류 발생"); return }
+        switch emotion {
+        case .veryHappy:
+            resultLabels[index].text = "\(emotionValue(emotion: emotion))"
+        case .happy:
+            resultLabels[index].text = "\(emotionValue(emotion: emotion))"
+        case .soso:
+            resultLabels[index].text = "\(emotionValue(emotion: emotion))"
+        case .bad:
+            resultLabels[index].text = "\(emotionValue(emotion: emotion))"
+        case .sad:
+            resultLabels[index].text = "\(emotionValue(emotion: emotion))"
+        }
+        
+    }
+    
+    func emotionResetAll(){
         UserDefaults.standard.set(0, forKey: Emotion.getString(.veryHappy)())
         UserDefaults.standard.set(0, forKey: Emotion.getString(.happy)())
         UserDefaults.standard.set(0, forKey: Emotion.getString(.soso)())
@@ -71,12 +118,13 @@ class ResultViewController: UIViewController {
         UserDefaults.standard.set(0, forKey: Emotion.getString(.sad)())
         setUpLabel()
     }
-    func randomColor() -> UIColor {
-            let red = CGFloat.random(in: 0...1)
-            let green = CGFloat.random(in: 0...1)
-            let blue = CGFloat.random(in: 0...1)
-            let color = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
-            return color
-        }
+    
+func randomColor() -> UIColor {
+    let red = CGFloat.random(in: 0...1)
+    let green = CGFloat.random(in: 0...1)
+    let blue = CGFloat.random(in: 0...1)
+    let color = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    return color
+}
 
 }
